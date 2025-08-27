b86f3a52233e1a849b196ce6f9e7705b31136813
redwind419 <johnlloydmanigoscamaso@gmail.com>
2025-08-27 15:34:11 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBKXMb6TaUMlYcZNLu8SZ4S-N9FVZaUpA",
+  authDomain: "spawnpoint-a5fe4.firebaseapp.com",
+  projectId: "spawnpoint-a5fe4",
+  storageBucket: "spawnpoint-a5fe4.firebasestorage.app",
+  messagingSenderId: "929399361204",
+  appId: "1:929399361204:web:5c17f13653657a9bac91e1"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
