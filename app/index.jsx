36d1fe71bd174b12538d8b7b7afdc105b08b0423
redwind419 <johnlloydmanigoscamaso@gmail.com>
2025-08-27 15:38:35 +0800

    Firebase Configuration

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        SVY - G O A L S
+        SVY - G A M E S
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
