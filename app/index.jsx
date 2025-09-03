45bf7f0a0aca0b25a9666315fb0f2eb92e3b1dff
redwind419 <johnlloydmanigoscamaso@gmail.com>
2025-09-03 15:12:16 +0800

    Create Goals

--- app/index.jsx
+++ app/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         View Your Goals
       </Link>
       <Link style={styles.link} href="/goals/create">
-        Add a New Goal
+        Add a New Goals
       </Link>
     </View>
   )
