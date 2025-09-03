9cad3e8325d92c2be0bf5380dd3f78033e52e1d0
sheenypon <johnlloydmanigoscamaso@gmail.com>
2025-09-03 15:25:12 +0800

    Create Goals

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        SV - G O A L S
+        SVPY - G O A L S
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
